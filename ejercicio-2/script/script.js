4388567265aea1fba597b329b8111cc1ac67936d
David Gómez Lozano <125882624+DavidGom1@users.noreply.github.com>
2026-01-28 13:07:29 +0100

    feat: Añade estructura base HTML, CSS y JS para mostrar bicicletas
    Se crea la estructura inicial del proyecto con HTML, CSS para el estilo, y JS para la lógica de la aplicación.

---
+++ ejercicio-2/script/script.js
@@ -0,0 +1,54 @@
+/*
+*   VARIABLES Y OBJETOS DEL DOM
+*/
+var consulta = new XMLHttpRequest();
+const contenedorBicis = document.getElementById('bicicletas');
+const selectCategoria = document.getElementById('selectCategoria');
+let urlBase = 'http://api.raulserranoweb.es/rest.php';
+
+
+// CONSULTA Y PETICION
+function pedirDatos(url){
+    consulta.onreadystatechange = procesarRespuesta;
+    consulta.open('GET', url, true);
+    consulta.send(null);
+}
+
+// USO DE DATOS
+function usoDatos(d){
+    contenedorBicis.innerHTML = '';
+    d.forEach(e => {
+        console.log(e)
+        const contenedorBici = document.createElement('div');
+        contenedorBici.classList = 'bici';
+        const imagen = document.createElement('img');
+        const nombre = document.createElement('p');
+        const desc = document.createElement('p');
+        const cat = document.createElement('p');
+        imagen.src = 'http://api.raulserranoweb.es/imagenes_art/' + e.cod;
+        nombre.innerHTML = '<b>Nombre: </b>' + e.nom;
+        desc.innerHTML = '<b>Descripción: </b>' + e.des;
+        cat.innerHTML = '<b>Categoria: </b>' + e.cat;
+        contenedorBici.append(imagen, nombre, desc, cat);
+        contenedorBicis.appendChild(contenedorBici);
+    });
+}
+
+// PROCESAMIENTO DE RESPUESTA
+function procesarRespuesta(){
+    if(consulta.readyState == 4 && consulta.status == 200){
+        let data = JSON.parse(consulta.responseText);
+        usoDatos(data);
+    }
+}
+
+
+/*
+*   PROCESAMIENTO
+*/
+pedirDatos(urlBase);
+
+selectCategoria.addEventListener('change', (e) => {
+    let cat = e.target.value;
+    pedirDatos(cat != 'Todas' ? urlBase + '?cat=' + cat : urlBase);
+});
