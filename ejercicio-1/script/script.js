e074a4a2ae4ac276783546c472949d4a37233c11
David Gómez Lozano <125882624+DavidGom1@users.noreply.github.com>
2026-01-30 20:15:06 +0100

    refactor: Refactoriza y documenta el código de los scripts
    Se ha refactorizado el código para mejorar la legibilidad y se ha añadido documentación.

--- ejercicio-1/script/script.js
+++ ejercicio-1/script/script.js
@@ -1,38 +1,37 @@
 /*
 *   VARIABLES Y OBJETOS DEL DOM
 */
-const url = 'https://jsonplaceholder.typicode.com/users';
-var consulta = new XMLHttpRequest();
-const cuerpoTabla = document.getElementById('cuerpoTabla');
-const botonBuscar = document.getElementById('btnBuscar');
-const inputBuscar = document.getElementById('inputBuscar');
+const url = 'https://jsonplaceholder.typicode.com/users';   // guardo la url de la API
+const cuerpoTabla = document.getElementById('cuerpoTabla');  // guardo el cuerpo de la tabla
+const botonBuscar = document.getElementById('btnBuscar');    // guardo el boton de buscar
+const inputBuscar = document.getElementById('inputBuscar');  // guardo el input de buscar
 let datos = [];
 
 
 // CONSULTA Y PETICION
-function pedirDatos() {
-    fetch(url)
-        .then(response => {
-            if (response.status == 200) {
-                return response.json();
+function pedirDatos() {  // funcion que pedira los datos, lo voy a probar aqui como dice el contenido, y en el 2 lo haré a mi manera
+    fetch(url)  // pido los datos
+        .then(response => {  // proceso la respuesta
+            if (response.status == 200) {  // si la respuesta es correcta
+                return response.json();  // devuelvo los datos
             } else {
-                throw "Respuesta incorrecta del servidor.";
+                throw "Respuesta incorrecta del servidor.";  // si no es correcta, lanzo un error
             }
         })
-        .then(data => {
-            datos = data;
-            usoDatos(data);
+        .then(data => {  // proceso los datos   
+            datos = data;   // guardo los datos en la global para luego usarlos en el filtro
+            usoDatos(data);  // uso los datos
         })
-        .catch(err => {
-            console.log(err);
+        .catch(err => {  // proceso el error
+            console.log(err);  // muestro el error
         })
 }
 
 // USO DE DATOS
-function usoDatos(d) {
-    d.forEach(e => {
-        cuerpoTabla.innerHTML += 
-        `<tr>
+function usoDatos(d) {  // funcion que usara los datos
+    d.forEach(e => {  // recorro el array de datos
+        cuerpoTabla.innerHTML +=    // voy añadiendo filas a la tabla
+        `<tr>   
             <td>${e.name}</td>
             <td>${e.address.street}</td>
             <td>${e.address.city}</td>
@@ -42,15 +41,15 @@ function usoDatos(d) {
 
 
 // PROCESAMIENTO
-(async () => {
-    const usuarios = await pedirDatos();
-    if(usuarios) usoDatos(usuarios);
+(async () => {  // funcion asincrona que podré llamar en cualquier momento
+    const usuarios = await pedirDatos();  // pido los datos
+    if(usuarios) usoDatos(usuarios);  // uso los datos
 })();
 
-botonBuscar.addEventListener('click', (e) => {
-    e.preventDefault();
-    let texto = inputBuscar.value.trim();
-    let datosFiltrados = datos.filter(e => e.name.toLowerCase().includes(texto.toLowerCase()));
-    cuerpoTabla.innerHTML = '';
-    usoDatos(datosFiltrados);
+botonBuscar.addEventListener('click', (e) => {  // evento click para el boton de buscar
+    e.preventDefault();  // evito el comportamiento por defecto
+    let texto = inputBuscar.value.trim();  // guardo el texto del input
+    let datosFiltrados = datos.filter(e => e.name.toLowerCase().includes(texto.toLowerCase()));  // filtro los datos
+    cuerpoTabla.innerHTML = '';  // vacio la tabla
+    usoDatos(datosFiltrados);  // uso los datos filtrados
 });
--- ejercicio-2/script/script.js
+++ ejercicio-2/script/script.js
@@ -1,58 +1,49 @@
 /*
 *   VARIABLES Y OBJETOS DEL DOM
 */
-const contenedorBicis = document.getElementById('bicicletas');
-const selectCategoria = document.getElementById('selectCategoria');
-const urlBase = 'https://api.raulserranoweb.es/rest.php';
-const urlImg = 'https://api.raulserranoweb.es/imagenes_art/';
+const contenedorBicis = document.getElementById('bicicletas');  // guardo el contenedor de bicis
+const selectCategoria = document.getElementById('selectCategoria');  // guardo el select de categorias
+const urlBase = 'https://api.raulserranoweb.es/rest.php';  // guardo la url base de la API
+const urlImg = 'https://api.raulserranoweb.es/imagenes_art/';  // guardo la url de las imagenes
 
 
 // CONSULTA Y PETICION
 async function pedirDatos(url){ // funcion asincrona que podré llamar en cualquier momento
-    const response = await fetch(url); //
+    const response = await fetch(url); // guardo la respuesta de la consulta a la API
 
-    if(!response.ok) throw new Error('Error recibiendo datos');
+    if(!response.ok) throw new Error('Error recibiendo datos'); // si la respuesta no es correcta, lanzo un error
 
-    const datos = await response.json();
-    return datos;
+    const datos = await response.json(); // guardo los datos de la respuesta
+    return datos;   // devuelvo los datos
 }
 
 // USO DE DATOS
 function usoDatos(d){
-    contenedorBicis.innerHTML = '';
-    d.forEach(e => {
-        const contenedorBici = document.createElement('div');
-        contenedorBici.classList = 'bici';
-        const imagen = document.createElement('img');
-        const nombre = document.createElement('p');
-        const desc = document.createElement('p');
-        const cat = document.createElement('p');
-        imagen.src = urlImg + e.cod;
-        nombre.innerHTML = '<b>Nombre: </b>' + e.nom;
-        desc.innerHTML = '<b>Descripción: </b>' + e.des;
-        cat.innerHTML = '<b>Categoria: </b>' + e.cat;
-        contenedorBici.append(imagen, nombre, desc, cat);
-        contenedorBicis.appendChild(contenedorBici);
+    contenedorBicis.innerHTML = ''; // vacio el contenedor de bicis
+    d.forEach(e => {    // recorro el array de datos
+        const contenedorBici = document.createElement('div'); // creo un div para cada bici
+        contenedorBici.classList = 'bici'; // le doy la clase bici
+        const imagen = document.createElement('img'); // creo un img para la imagen
+        const nombre = document.createElement('p'); // creo un p para el nombre
+        const desc = document.createElement('p'); // creo un p para la descripcion
+        const cat = document.createElement('p'); // creo un p para la categoria
+        imagen.src = urlImg + e.cod; // le doy la ruta de la imagen
+        nombre.innerHTML = '<b>Nombre: </b>' + e.nom; // le doy el nombre
+        desc.innerHTML = '<b>Descripción: </b>' + e.des; // le doy la descripcion
+        cat.innerHTML = '<b>Categoria: </b>' + e.cat; // le doy la categoria
+        contenedorBici.append(imagen, nombre, desc, cat); // añado los elementos al contenedor
+        contenedorBicis.appendChild(contenedorBici); // añado el contenedor al contenedor principal
     });
 }
-
-// PROCESAMIENTO DE RESPUESTA
-function procesarRespuesta(){
-    if(consulta.readyState == 4 && consulta.status == 200){
-        let data = JSON.parse(consulta.responseText);
-        usoDatos(data);
-    }
-}
-
 
 /*
 *   PROCESAMIENTO
 */
-(async () => {
-    usoDatos( await pedirDatos(urlBase) );
+(async () => {  // funcion asincrona que podré llamar en cualquier momento
+    usoDatos( await pedirDatos(urlBase) ); // uso los datos
 })();
 
-selectCategoria.addEventListener('change', async (e) => {
-    let cat = e.target.value;
-    usoDatos( await pedirDatos(cat != 'Todas' ? urlBase + '?cat=' + cat : urlBase));
+selectCategoria.addEventListener('change', async (e) => { // evento change para el select
+    let cat = e.target.value; // guardo el valor del select
+    usoDatos( await pedirDatos(cat != 'Todas' ? urlBase + '?cat=' + cat : urlBase) ); // uso los datos
 })
